4d097e0a0baedcf108655dbef7d26590d8dbe6be
Kabalan Elahmadie <kahmadieh@yahoo.com>
2019-05-17 17:23:19 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,5 @@
+var chocolateBars = ["snikers", "hundred grand","kitkat", "skittles" ];
+["Mars", ...chocolateBars]
+function addElementToBeginningOfArray(array, element){
+return [element, ...array];
+}
